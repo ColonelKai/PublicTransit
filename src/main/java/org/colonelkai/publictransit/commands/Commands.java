8e4e8d852fdb31af9b0ff864724fe27e377f7e1c
Colonel Kai <kayraacar@hotmail.co.jp>
2023-09-16 13:50:38 +0900

    woooweewoowee

---
+++ src/main/java/org/colonelkai/publictransit/commands/Commands.java
@@ -0,0 +1,13 @@
+package org.colonelkai.publictransit.commands;
+
+import org.bukkit.command.CommandExecutor;
+
+public class Commands {
+    public static void register() {
+
+    }
+
+    public static void register(String command, CommandExecutor executor) {
+
+    }
+}
