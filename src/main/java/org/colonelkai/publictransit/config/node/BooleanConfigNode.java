4b4afb20a626bebb71d2305ec130e9b22e245792
mosemister <nekronmoseley@gmail.com>
2023-10-10 15:59:40 +0100

    Weight support

---
+++ src/main/java/org/colonelkai/publictransit/config/node/BooleanConfigNode.java
@@ -0,0 +1,24 @@
+package org.colonelkai.publictransit.config.node;
+
+import org.core.config.ConfigurationNode;
+import org.core.config.ConfigurationStream;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public class BooleanConfigNode extends AbstractConfigNode<Boolean> {
+
+    public BooleanConfigNode(ConfigurationNode path, boolean defaultValue) {
+        super(path, defaultValue);
+    }
+
+    @Override
+    protected @NotNull Optional<Boolean> get(@NotNull ConfigurationStream stream) {
+        return stream.getBoolean(this.getPath());
+    }
+
+    @Override
+    protected void set(@NotNull ConfigurationStream stream, @NotNull Boolean value) {
+        stream.set(this.getPath(), value);
+    }
+}
