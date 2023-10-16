52cbbf3d5e17d91edf285e771bde70ecd177ab43
ColonelKai <kayraacar@hotmail.co.jp>
2023-10-16 15:56:12 +0900

    Removed duplicate entry point for config

--- src/main/java/org/colonelkai/publictransit/config/PublicTransitConfigNodes.java
+++ src/main/java/org/colonelkai/publictransit/config/PublicTransitConfigNodes.java
@@ -7,29 +7,27 @@ import org.core.config.ConfigurationNode;
 
 interface PublicTransitConfigNodes {
 
-    // CONFIG FILES
-    final Config MAIN_CONFIG = PublicTransit.getPlugin().getConfig();
 
     // General Settings
     // Max distance a player needs to be from a node to interact with it
     DoubleConfigNode PLAYER_DISTANCE_FROM_NODE = new DoubleConfigNode(new ConfigurationNode(
-            "node", "distance", "player"), 10, MAIN_CONFIG);
+            "node", "distance", "player"), 10, PublicTransit.getPlugin().getConfig());
 
     // Node Default Settings
     DoubleConfigNode DEFAULT_NODE_PRICE = new DoubleConfigNode(new ConfigurationNode(
-            "node", "default", "price"), 100, MAIN_CONFIG);
+            "node", "default", "price"), 100, PublicTransit.getPlugin().getConfig());
     DoubleConfigNode DEFAULT_NODE_TIME = new DoubleConfigNode(new ConfigurationNode(
-            "node", "default", "time"), 15, MAIN_CONFIG);
+            "node", "default", "time"), 15, PublicTransit.getPlugin().getConfig());
 
     // Line Default Settings
 
     CostTypeConfigNode DEFAULT_LINE_COST_TYPE = new CostTypeConfigNode(new ConfigurationNode(
-            "line", "default", "costtype"), CostType.FLAT_RATE, MAIN_CONFIG);
+            "line", "default", "costtype"), CostType.FLAT_RATE, PublicTransit.getPlugin().getConfig());
 
     // Economy Settings
 
     BooleanConfigNode USE_DEPOSIT_ACCOUNT = new BooleanConfigNode(new ConfigurationNode(
-            "economy", "depositaccount", "use"), false, MAIN_CONFIG);
+            "economy", "depositaccount", "use"), false, PublicTransit.getPlugin().getConfig());
     StringConfigNode DEPOSIT_ACCOUNT = new StringConfigNode(new ConfigurationNode(
-            "economy", "depositaccount", "name"), "Government", MAIN_CONFIG);
+            "economy", "depositaccount", "name"), "Government", PublicTransit.getPlugin().getConfig());
 }
